502ce9c667ad00cf4d528d81c256db776869966d
opperator <julie.e.oppermann@gmail.com>
2016-01-31 17:03:54 -0500

    commiting for deploying

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -8,6 +8,7 @@ module.exports = function (app) {
     var root = app.getValue('projectRoot');
 
     var npmPath = path.join(root, './node_modules');
+    var bowerPath = path.join(root, './bower_components');
     var publicPath = path.join(root, './public');
     var imagesPath = path.join(root, './browser/images')
     var browserPath = path.join(root, './browser');
@@ -17,6 +18,7 @@ module.exports = function (app) {
     app.use(express.static(publicPath));
     app.use(express.static(imagesPath));
     app.use(express.static(browserPath));
+    app.use(express.static(bowerPath));
 
 
 
